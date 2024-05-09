47432d349a44516865385b4b8d8ba1f3eb5c5bc8
leonne <minterbottsyt@gmail.com>
2024-05-09 16:57:10 +0300

    Done with the AccountContainer

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [originalTransactions, setOriginalTransactions] = useState([]);
+  const getTransactions = () => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:3000/transactions", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setTransactions(result);
+        setOriginalTransactions(result);
+      })
+      .catch((error) => console.error(error));
+  };
+  useEffect(() => {
+    getTransactions();
+  }, []);
+
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search
+        transactions={transactions}
+        setTransactions={setTransactions}
+        originalTransactions={originalTransactions}
+      />
+      <AddTransactionForm
+        transactions={transactions}
+        setTransactions={setTransactions}
+        getTransactions={getTransactions}
+      />
+      <TransactionsList transactions={transactions} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function Search() {
+function Search(props) {
+  const { transactions, setTransactions, originalTransactions } = props;
+  const [search, setSearch] = useState("");
+
+  const doSearch = (e) => {
+    console.log(e.target.value.toLowerCase());
+    let s = e.target.value.toLowerCase();
+    console.log(s);
+    setSearch(s);
+    if (s.length < 4) {
+      setTransactions(originalTransactions);
+
+      return;
+    }
+    let filteredTransaction = [];
+    for (let i = 0; i < transactions.length; i++) {
+      console.log(transactions[i]);
+      let trans = transactions[i];
+      if (!trans.description) {
+        continue;
+      }
+      if (!trans.category) {
+        continue;
+      }
+      let description = trans.description.toLowerCase();
+      let category = trans.category.toLowerCase();
+      if (description.includes(s) || category.includes(s)) {
+        filteredTransaction.push(trans);
+      }
+    }
+    if (filteredTransaction.length > 0) {
+      setTransactions(filteredTransaction);
+      return;
+    }
+    setTransactions(originalTransactions);
+  };
+
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+        onChange={(e) => {
+          doSearch(e);
+        }}
+        value={search}
       />
       <i className="circular search link icon"></i>
     </div>
